ec61d1fa880a1e84dc87c2e50ddca5b07c41b671
peterhoward42 <peterhoward42@gmail.com>
2021-02-04 08:28:12 +0000

    Put in a reactive statement

--- src/Inner.svelte
+++ src/Inner.svelte
@@ -1,7 +1,14 @@
 <script>
     export let sex ;
     export let age ;
+
+    // A reactive variable.
+    // 
+    // Preceding a load-time statement with $: causes the statement
+    // to be re-run whenever the objects it refers to have changed.
+    $: doubled = 2 * age;
 </script>
 
 <!-- A MVP component to show consumption of props -->
 <p>Just a paragraph with this sex: {sex}, and this age: {age}</p>
+<p>And age doubled: {doubled}</p>
