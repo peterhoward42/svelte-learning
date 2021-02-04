4551dc1eb6461ebe4fc46416b6a48e93b34e7356
peterhoward42 <peterhoward42@gmail.com>
2021-02-04 12:45:06 +0000

    Each i.e. loop directive

--- src/Inner.svelte
+++ src/Inner.svelte
@@ -1,9 +1,10 @@
 <script>
-    export let sex ;
-    export let age ;
+    export let sex;
+    export let age;
+    const fruits = ['apple', 'banana', 'orange'];
 
     // A reactive variable.
-    // 
+    //
     // Preceding a load-time statement with $: causes the statement
     // to be re-run whenever the objects it refers to have changed.
     $: doubled = 2 * age;
@@ -12,3 +13,12 @@
 <!-- A MVP component to show consumption of props -->
 <p>Just a paragraph with this sex: {sex}, and this age: {age}</p>
 <p>And age doubled: {doubled}</p>
+<p />
+<p>Now an each loop</p>
+{#each fruits as fruit}
+    <ul>
+        <li>
+            Item is: {fruit}
+        </li>
+    </ul>
+{/each}
