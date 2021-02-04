1d8c33dc1aa0b600306ec320901409abf0ac9ed0
peterhoward42 <peterhoward42@gmail.com>
2021-02-04 13:01:01 +0000

    Handle mouse move events

--- src/App.svelte
+++ src/App.svelte
@@ -2,21 +2,30 @@
 	export let name;
 
 	const innerProps = {
-		sex: 'male',
-		age: 99
+		sex: "male",
+		age: 99,
 	};
 
-import Inner from './Inner.svelte';
-	import InnerCpt from './Inner.svelte';
+	import Inner from "./Inner.svelte";
+	import InnerCpt from "./Inner.svelte";
 
+	function handleMouseMoved(event) {
+		console.log('Received mouse move evt')
+	}
 </script>
 
 <main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+	<!-- Receive mouse movement -->
+	<div on:mousemove={handleMouseMoved}>
+		<h1>Hello {name}!</h1>
+		<p>
+			Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to
+			learn how to build Svelte apps.
+		</p>
 
-	<!-- How to compose with another component, using spread for props -->
-	<InnerCpt {...innerProps} ></InnerCpt>
+		<!-- How to compose with another component, using spread for props -->
+		<InnerCpt {...innerProps} />
+	</div>
 </main>
 
 <style>
