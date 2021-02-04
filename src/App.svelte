b2d75488446d4df22e50c3c4b4b3ceb4ea7d69c4
peterhoward42 <peterhoward42@gmail.com>
2021-02-04 08:20:04 +0000

    Put in a nested component with props

--- src/App.svelte
+++ src/App.svelte
@@ -1,10 +1,22 @@
 <script>
 	export let name;
+
+	const innerProps = {
+		sex: 'male',
+		age: 99
+	};
+
+import Inner from './Inner.svelte';
+	import InnerCpt from './Inner.svelte';
+
 </script>
 
 <main>
 	<h1>Hello {name}!</h1>
 	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+
+	<!-- How to compose with another component, using spread for props -->
+	<InnerCpt {...innerProps} ></InnerCpt>
 </main>
 
 <style>
---
+++ src/Inner.svelte
@@ -0,0 +1,7 @@
+<script>
+    export let sex ;
+    export let age ;
+</script>
+
+<!-- A MVP component to show consumption of props -->
+<p>Just a paragraph with this sex: {sex}, and this age: {age}</p>
